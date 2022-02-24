24af45d356a79c0cf6c01d7faca23a38b5d664f2
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2022-02-24 11:24:55 +0530

    todo(): avatar

--- src/avatar/avatar.controller.ts
+++ src/avatar/avatar.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get, Header, Param, Query, Response as Res } from '@nestjs/common'
-import { AvatarService } from './avatar.service'
+import { Controller, Get, Header, Param, Query, Response as Res } from "@nestjs/common";
+import { AvatarService } from "./avatar.service";
 import { Response } from 'express'
 @Controller('avatar')
 export class AvatarController {
+
     constructor(private avatarService: AvatarService) {}
 
     @Get('/:seed')
     public generateAvatar(@Param('seed') seed: string): string {
-        return this.avatarService.generateAvatar(seed)
+        return this.avatarService.generateAvatar(seed);
     }
 
-    @Get('/asImage/:seed')
+    @Get('/asImage/:sebed') 
     @Header('Content-Type', 'image/svg+xml')
-    public async asImage(@Param('seed') seed: string, @Res() res: Response) {
+    public async asImage(@Param('seed') seed: string, @Res() res: Response){
         const img = await this.avatarService.asImage(seed)
         return res.send(img)
     }
